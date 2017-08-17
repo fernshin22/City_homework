a03679c4fcdd5a4c414baf5d720c198439440b1c
fernshin22 <ferntak22@Hotmail.com>
2017-08-16 18:41:10 -0700

    Fernanda City Dropdown HW

--- starter_code/js/index.js
+++ starter_code/js/index.js
@@ -8,43 +8,78 @@
 
  $(document).ready(function(){
 
-	$("#submit-btn").click(showBackgroundLA);
+	var cities = ["NYC", "SF", "LA", "ATX", "SYD"];//creates an array of city names and assigns it as the value to the newly created "cities" variable
+	console.log(cities);
+	
+	cities.forEach(function(element, index){
+			console.log(element, index);
+			$("select").append("<option>"+element+"</option>");
+		})
+
+
+	
+// $("select").on("click", showBackgroundLa)
+
+	$("select").change(showBackgroundLa);
+		function showBackgroundLa(){
+			var city = $("select").val();
+			console.log("selection made", city) 
+
+			/*
+			store the user's selection in a variable
+
+			if they choose new york, change bg to nyc
+			*/
+			if(city=="NYC"){
+				$("body").attr("class", "nyc");
+			}
+
+			else if(city=="SF"){
+				$("body").attr("class", "sf");
+			}
+			else if(city=="LA"){
+				$("body").attr("class", "la");
+			}
+			else if(city=="ATX"){
+				$("body").attr("class", "austin");
+			}
+		
+			else if(city=="SYD"){
+				$("body").attr("class", "sydney");
+			}
+		
+		}
 
 
 
 
 
- 	function showBackgroundLA(){
- 		event.preventDefault(); 
- 		var city = $("#city-type").val();
-		$("#city-type").val("");
- 		
+	//  	function showBackgroundLA(){
+	//  		event.preventDefault(); 
+	//  		var city = $("select").val();
+	// 		$("#city-type").val("");
+	 		
 
- 		if (city == "los angeles" || city == "la" || city == "lax"){
- 		$("body").attr("class", "la");
-    	}	
-		else if (city == "new york" || city == "nyc" || city == "new york-city"){
- 		$("body").attr("class", "nyc");
-    	}	
-    	else if (city == "san francisco" || city == "sf" || city == "bay area"){
- 		$("body").attr("class", "sf");
-    	}
-    	else if (city == "austin" || city == "atx"){
- 		$("body").attr("class", "austin");
-    	}
-    	else if (city == "sydney" || city == "syd") {
- 		$("body").attr("class", "sydney");
-    	}
-    	
-    	
-
-
-	}
+	//  		if (city == "los angeles" || city == "la" || city == "lax"){
+	//  		$("body").attr("class", "la");
+	//     	}	
+	// 		else if (city == "new york" || city == "nyc" || city == "new york-city"){
+	//  		$("body").attr("class", "nyc");
+	//     	}	
+	//     	else if (city == "san francisco" || city == "sf" || city == "bay area"){
+	//  		$("body").attr("class", "sf");
+	//     	}
+	//     	else if (city == "austin" || city == "atx"){
+	//  		$("body").attr("class", "austin");
+	//     	}
+	//     	else if (city == "sydney" || city == "syd") {
+	//  		$("body").attr("class", "sydney");
+	//     	}
+	    	
+	//     }
 
 
+		
+ }); //end of document.ready
 
 
- })
-
-
-// Barry: Not sure why this isn't working. I got rid of all the semicolons before the brackets in the if/else statements.
